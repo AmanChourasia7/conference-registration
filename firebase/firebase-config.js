01189613732fc8639cfd0d75c6c97dc75a434380
Aman Chourasia <76176138+AmanChourasia7@users.noreply.github.com>
2026-03-09 07:00:29 +0530

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApcPdCFJQ5Fhm3LDsiLm-jN8FvspG13Bw",
+  authDomain: "conference-registration-iitp.firebaseapp.com",
+  projectId: "conference-registration-iitp",
+  storageBucket: "conference-registration-iitp.firebasestorage.app",
+  messagingSenderId: "241554962829",
+  appId: "1:241554962829:web:c09ea2b590c7484be465c4",
+  measurementId: "G-LLF7WVGHDM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app };
 
