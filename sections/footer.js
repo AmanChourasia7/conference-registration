617ea68cd1a47d5432e0db8cff61a876f3d7fc66
Aman Chourasia <76176138+AmanChourasia7@users.noreply.github.com>
2026-03-28 23:32:04 +0000

    refined and built more pages.

---
+++ sections/footer.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const footerHTML = `
+        <td id="footer-left">
+            &copy; 2027 QML Conference
+        </td>
+        <td id="footer-right">
+           <a href="login.html">Register</a>
+            <a href="login.html">Login</a>
+            <a href="report.html">Report!</a>
+        </td>
+    `;
+
+    const footerRow = document.getElementById("footer-row");
+    if (footerRow) {
+        footerRow.innerHTML = footerHTML;
+    }
+});
