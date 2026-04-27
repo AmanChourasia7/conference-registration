af5bf288e45d59fb0791515b4ed97d1135e1699e
Aman Chourasia <76176138+AmanChourasia7@users.noreply.github.com>
2026-04-28 00:08:59 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,12 @@
 import { app } from "../firebase/firebase-config.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 document.querySelector("button").addEventListener("click", async () => {
+
   const email = document.querySelector("input[type='email']").value;
   const password = document.querySelector("input[type='password']").value;
 
@@ -11,11 +14,25 @@ document.querySelector("button").addEventListener("click", async () => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    // redirect after login
-    window.location.href = "admin.html";
+    const userDoc = await getDoc(doc(db, "users", user.uid));
+
+    if (!userDoc.exists()) {
+      alert("User data not found");
+      return;
+    }
+
+    const role = userDoc.data().role;
+
+    // 🔑 ROLE BASED REDIRECT
+    if (role === "admin") {
+      window.location.href = "admin.html";
+    } else {
+      window.location.href = "dashboard.html";
+    }
 
   } catch (err) {
     alert("Login failed");
     console.error(err);
   }
+
 });
